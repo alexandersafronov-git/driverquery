417099addc26defee86ca23afd2c6f0f8cc47fac
asafronov <asafronov@usergate.com>
2026-04-24 14:44:04 +0300

    add arguments

--- driverquery.py
+++ driverquery.py
@@ -3,22 +3,25 @@ import subprocess
 import numpy as np
 import pandas as pd
 from tabulate import tabulate
-
+import argparse
+import csv
 logger = logging.getLogger('driverquery')
 
 class DriverQuery:
     """Runs "driverquery" utility and save the results to a file,
        and then open this file and output only drivers with
        requested driver type ("File System " by default)."""
-    def __init__(self, filename='drivers.csv', driver_type='File System '):
+    def __init__(self, filename='drivers.csv', driver_type='File System ', format_output='TABLE'):
         """ Gets data from "driverquery" and save/load into csv file.
 
         Args:
             filename: csv file name, optional.
             driver_type:  requested driver type for printing, optional "File System " by default.
+            format_output: output format "TABLE" (default) or "CSV"
         """
         self.filename = filename
         self.driver_type = driver_type
+        self.format_output = format_output
         drivers = self.get_data()
         self.save_data(drivers)
         self.data = self.load_data()
@@ -47,11 +50,24 @@ class DriverQuery:
         df = df.fillna('')
         subset = df[['Module Name', 'Display Name', 'Driver Type', 'Link Date']]
         filtered = subset[subset['Driver Type'] == self.driver_type]
-        # On Windows replace method for NaN doesn't work properly for columns with non-numeric values
-        return str(tabulate(filtered.replace(np.nan, None), headers='keys', showindex=False))
+        if self.format_output == 'TABLE':
+            # On Windows replace method for NaN doesn't work properly for columns with non-numeric values
+            return str(tabulate(filtered.replace(np.nan, None), headers='keys', showindex=False))
+        elif self.format_output == 'CSV':
+            return filtered.to_csv(index=False, quoting=csv.QUOTE_ALL)
+        else:
+            raise ValueError('unknown format output: {:s}'.format(self.format_output))
 
 
 if __name__ == '__main__':
-    drivers = DriverQuery()
+    example_text = '''Usage example:
+    driverquery.py --driver_type="Kernel " --fo CSV
+    '''
+    parser = argparse.ArgumentParser(description='driverquery', epilog=example_text)
+    parser.add_argument('-d', '--driver_type', type=str, help='prints only drivers with certain driver type, '
+                                                        'default "Files System "', default='File System ')
+    parser.add_argument('-f', '--fo', type=str, help='output format: TABLE (by default) or CSV', default='TABLE')
+    args = parser.parse_args()
+    drivers = DriverQuery(driver_type=args.driver_type, format_output=args.fo)
     print(drivers)
 
--- test_driverquery.py
+++ test_driverquery.py
@@ -2,11 +2,13 @@ from unittest.mock import patch
 from unittest.mock import Mock
 from driverquery import DriverQuery
 
-expected_output = """Module Name    Display Name    Driver Type    Link Date
+expected_output_table = """Module Name    Display Name    Driver Type    Link Date
 -------------  --------------  -------------  -----------
 AppvStrm       AppvStrm        File System"""
 
-
+expected_output_csv = """"Module Name","Display Name","Driver Type","Link Date"
+"AppvStrm","AppvStrm","File System ",""
+"""
 def get_driversquery_stdout():
     with open('tests/driversquery_stdout.csv', 'r', encoding='utf-8') as output:
         result = output.read()
@@ -20,8 +22,10 @@ def test_run_driverquery(mock_run):
     mock_run.return_value = mock_result  # Set the return value of mock_run
     # test mock
     assert DriverQuery().get_data() == get_driversquery_stdout()
-    # test command output
+    # test table format output
     drivers = DriverQuery()
-    output = str(drivers)
-    assert output == expected_output
+    assert str(drivers) == expected_output_table
+    # test csv format output
+    drivers = DriverQuery(format_output='CSV')
+    assert str(drivers) == expected_output_csv
 
