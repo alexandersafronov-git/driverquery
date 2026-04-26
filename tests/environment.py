ca9a5bf6817451268ce5fab162264df627d57bfa
asafronov <asafronov@usergate.com>
2026-04-26 13:43:32 +0300

    parametrize test

--- tests/environment.py
+++
@@ -1,4 +0,0 @@
-expected_output_table = """Module Name    Display Name    Driver Type    Link Date
--------------  --------------  -------------  -----------
-AppvStrm       AppvStrm        File System"""
-
--- tests/test_driverquery.py
+++ tests/test_driverquery.py
@@ -2,7 +2,7 @@ from unittest.mock import patch
 from ..driverquery import DriverQuery
 import pytest
 from .conftest import get_driversquery_stdout
-from ..tests import environment
+
 
 @patch('subprocess.run')
 def test_run_driverquery(mock_run, mock_driverquery):
@@ -13,18 +13,14 @@ def test_run_driverquery(mock_run, mock_driverquery):
 
 
 @patch('subprocess.run')
-def test_driverquery_table(mock_run, mock_driverquery):
+@pytest.mark.parametrize("format_output,expected", [("TABLE", "tests/expected_output_table.txt"),
+                                                    ("CSV", 'tests/expected_output.csv')])
+def test_driverquery_output(mock_run, mock_driverquery, format_output, expected):
     # test table format output
     mock_run.return_value = mock_driverquery
-    drivers = DriverQuery()
-    assert str(drivers) == environment.expected_output_table
+    drivers = DriverQuery(format_output=format_output)
+    assert str(drivers).replace('\r', '') == str(get_driversquery_stdout(expected))
 
-@patch('subprocess.run')
-def test_driverquery_csv(mock_run, mock_driverquery):
-    # test csv format output
-    mock_run.return_value = mock_driverquery
-    drivers = DriverQuery(format_output='CSV')
-    assert str(drivers).replace('\r', '') == str(get_driversquery_stdout('tests/expected_output.csv'))
 
 @patch('subprocess.run')
 def test_driverquery_wrong_format_output(mock_run, mock_driverquery):
@@ -33,4 +29,3 @@ def test_driverquery_wrong_format_output(mock_run, mock_driverquery):
     drivers = DriverQuery(format_output='UNKNOWN')
     with pytest.raises(Exception) as e_info:
         str(drivers)
-
