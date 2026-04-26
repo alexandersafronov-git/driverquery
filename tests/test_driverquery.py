9093dfc09c0e1c86a6500cd8889f4661401e095d
asafronov <asafronov@usergate.com>
2026-04-26 08:07:46 +0300

    Move tests to folder

--- tests/test_driverquery.py
+++ tests/test_driverquery.py
@@ -1,11 +1,11 @@
 from unittest.mock import patch
-from driverquery import DriverQuery
+from ..driverquery import DriverQuery
 import pytest
-from conftest import get_driversquery_stdout
-import tests.environment
+from .conftest import get_driversquery_stdout
+from ..tests import environment
 
 
-@patch('driverquery.subprocess.run')
+@patch('subprocess.run')
 def test_run_driverquery(mock_run, mock_driverquery):
     # Create a mock subprocess result
     mock_run.return_value = mock_driverquery
@@ -13,21 +13,21 @@ def test_run_driverquery(mock_run, mock_driverquery):
     assert DriverQuery().get_data() == get_driversquery_stdout()
 
 
-@patch('driverquery.subprocess.run')
+@patch('subprocess.run')
 def test_driverquery_table(mock_run, mock_driverquery):
     # test table format output
     mock_run.return_value = mock_driverquery
     drivers = DriverQuery()
-    assert str(drivers) == tests.environment.expected_output_table
+    assert str(drivers) == environment.expected_output_table
 
-@patch('driverquery.subprocess.run')
+@patch('subprocess.run')
 def test_driverquery_csv(mock_run, mock_driverquery):
     # test csv format output
     mock_run.return_value = mock_driverquery
     drivers = DriverQuery(format_output='CSV')
-    assert str(drivers) == tests.environment.expected_output_csv
+    assert str(drivers) == environment.expected_output_csv
 
-@patch('driverquery.subprocess.run')
+@patch('subprocess.run')
 def test_driverquery_wrong_format_output(mock_run, mock_driverquery):
     # test wrong output format
     mock_run.return_value = mock_driverquery
